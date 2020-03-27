edcb101f048d4213ca9ec4a6922fd92a3a60362e
Phillip Clontz <PhillipAC718@gmail.com>
2020-03-27 17:09:39 -0500

    Created the basic functions of the Counter model

--- BackgroundProcessWpf/Models/Counter.cs
+++ BackgroundProcessWpf/Models/Counter.cs
@@ -6,5 +6,21 @@ namespace BackgroundProcessWpf.Models
 {
     class Counter
     {
+        public int Count { get; set; }
+
+        public Counter(int initialCount)
+        {
+            Count = initialCount;
+        }
+
+        /// <summary>
+        /// Increase the count by amount
+        /// </summary>
+        /// <param name="amount">The amount to increase the amount by</param>
+        /// <returns></returns>
+        public int Increment(int amount = 1)
+        {
+            return Count += amount;
+        }
     }
 }
